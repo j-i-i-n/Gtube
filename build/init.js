dec47ac6fe1b2745a7b4a68b60757c877c9b912b
j-i-i-n <53178900+j-i-i-n@users.noreply.github.com>
2020-07-24 14:01:07 +0900

    alalala

--- build/init.js
+++ build/init.js
@@ -18,7 +18,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 _dotenv["default"].config();
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 4000;
 
 var handleListening = function handleListening() {
   return console.log("\u2705 Listening on: http://localhost:".concat(PORT));
